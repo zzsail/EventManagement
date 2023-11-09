d7f70282bf6c3747ffe66baa8adc0a89a351ef2b
qjc <2401822066@qq.com>
2023-11-09 16:39:01 +0800

    添加奖项

--- EM-Controller/src/main/java/com/emt/AwardController.java
+++ EM-Controller/src/main/java/com/emt/AwardController.java
@@ -2,12 +2,10 @@ package com.emt;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.emt.composite.AwardComposite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +17,9 @@ public class AwardController {
 
     @Autowired
     private AwardService awardService;
+
+    @Autowired
+    private EventService eventService;
 
     private final Boolean IS_EXIST = true;
     private final Boolean IS_NOT_EXIST = false;
@@ -35,5 +36,32 @@ public class AwardController {
         Map<String, Object> map = new HashMap<>();
         map.put("items", pages);
         return Result.success(map);
+    }
+
+    //添加奖项
+    @PostMapping(path = "/create")
+    public Result saveAward(@RequestBody AwardComposite awardComposite){
+        LambdaQueryWrapper<Award> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(Award::getAwardName, awardComposite.getEventName());
+        lqw.eq(Award::getExist, IS_EXIST);
+        Award one = awardService.getOne(lqw);
+        if (one != null){
+            return Result.error("该奖项已存在");
+        }
+        Award award = new Award();
+        award.setAwardName(awardComposite.getAwardName());
+        award.setAwardDescription(awardComposite.getAwardDescription());
+        String eventName = awardComposite.getEventName();
+        LambdaQueryWrapper<Event> lqw2 = new LambdaQueryWrapper<>();
+        lqw2.eq(Event::getEventName, eventName);
+        lqw2.eq(Event::getExist, IS_EXIST);
+        Event one1 = eventService.getOne(lqw2);
+        if(one1 == null){
+            return Result.error("该赛事不存在");
+        }
+        award.setEventId(one1.getEventId());
+        awardService.save(award);
+        return Result.success();
+
     }
 }
---
+++ EM-Pojo/src/main/java/com/emt/composite/AwardComposite.java
@@ -0,0 +1,9 @@
+package com.emt.composite;
+
+import com.emt.Award;
+import lombok.Data;
+
+@Data
+public class AwardComposite extends Award {
+    private String eventName;
+}
