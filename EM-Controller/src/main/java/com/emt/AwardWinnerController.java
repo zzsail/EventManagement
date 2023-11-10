dd7046f69750beab9941325b77022637b0368b52
qjc <2401822066@qq.com>
2023-11-10 22:05:57 +0800

    获奖分页查询v0.1

--- EM-Controller/src/main/java/com/emt/AwardWinnerController.java
+++ EM-Controller/src/main/java/com/emt/AwardWinnerController.java
@@ -2,15 +2,15 @@ package com.emt;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.emt.composite.AwardWinnerComposite;
+import com.mysql.cj.jdbc.ha.LoadBalancedMySQLConnection;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.rmi.MarshalledObject;
+import java.util.*;
 
 @CrossOrigin
 @RestController
@@ -20,6 +20,9 @@ public class AwardWinnerController {
     private AwardWinnerService awardWinnerService;
 
     @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
     private AwardService awardService;
 
     private final Boolean IS_EXIST = true;
@@ -27,8 +30,41 @@ public class AwardWinnerController {
 
     //分页查询
     @GetMapping("/page")
-    public Result page(Integer pageNum, Integer pageSize, String awardWinnerName){
-        //不会
+    public Result page(Integer pageNum, Integer pageSize, Long awardId){
+        Page<AwardWinner> pages = new Page<>(pageNum, pageSize);
+        LambdaQueryWrapper<AwardWinner> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(Objects.nonNull(awardId), AwardWinner::getAwardId, awardId);
+        lqw.eq(AwardWinner::getExist, IS_EXIST);
+        List<AwardWinner> records = awardWinnerService.page(pages, lqw).getRecords();
+        List<AwardWinnerComposite> recordsComposite = new ArrayList<>();
+        records.stream().forEach(item -> {
+            AwardWinnerComposite awardWinnerComposite = new AwardWinnerComposite();
+            awardWinnerComposite.setAwardDate(item.getAwardDate());
+            //根据参赛者id获取参赛者姓名
+            LambdaQueryWrapper<Participant> lqw2 = new LambdaQueryWrapper<>();
+            lqw2.eq(Participant::getParticipantId, item.getParticipantId());
+            lqw2.eq(Participant::getExist, IS_EXIST);
+            Participant one = participantService.getOne(lqw2);
+            awardWinnerComposite.setParticipantName(one.getParticipantName());
+            //根据奖项id获取奖项名
+            LambdaQueryWrapper<Award> lqw3 = new LambdaQueryWrapper<>();
+            lqw3.eq(Award::getAwardId, item.getAwardId());
+            lqw3.eq(Award::getExist, IS_EXIST);
+            Award one1 = awardService.getOne(lqw3);
+            awardWinnerComposite.setAwardName(one1.getAwardName());
+            recordsComposite.add(awardWinnerComposite);
+        });
+        Page<AwardWinnerComposite> newPages = new Page<>();
+        BeanUtils.copyProperties(pages, newPages);
+        newPages.setRecords(recordsComposite);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("items", newPages);
+        return Result.success(map);
+    }
+
+    @PostMapping(path = "/create")
+    public Result saveAwardWinner(@RequestBody Participant participant){
         return null;
     }
 
--- EM-Pojo/src/main/java/com/emt/AwardWinner.java
+++ EM-Pojo/src/main/java/com/emt/AwardWinner.java
@@ -13,7 +13,7 @@ public class AwardWinner {
     @TableId(value = "winner_id", type = IdType.ASSIGN_ID)
     private Long winnerId;//获奖者ID
     private Long awardId;//奖项ID
-    private Participant participant;//参赛者ID
+    private Long participantId;//参赛者ID
     private LocalDateTime awardDate;//获奖日期
     private Boolean exist;//是否存在（1表示存在，0表示删除）
 }
